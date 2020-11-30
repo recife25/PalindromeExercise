64d7d12fd0fe1f66929c36bec201e9fbd8bac2bb
recife25 <felipe.j.ruiz64@gmail.com>
2020-11-30 17:58:26 -0500

    Added palindrome test

---
+++ PalindromeExercise/PalindromeExercise/Palindrome.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class Palindrome
+    {
+        public bool IsAPalindrome(string word)
+        {
+            //iteration of word
+            //reverse the word - hello - olleh
+            var reversed = "";
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                //reversed = reversed + word[i]
+                reversed += word[i];
+            }
+            //compare original with reverse word
+            if (reversed == word)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            //return palindrome
+            
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,23 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        public void MyTest(string word, bool expected)
         {
+            //arrange create instant of Palindrome class
+            var test = new Palindrome();
+            //act call the palidrome method
+            var actual = test.IsAPalindrome(word);
+            //assert - compare expected and actual
+            Assert.Equal(expected, actual);
+            
 
         }
     }
